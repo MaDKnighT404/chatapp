e6480bf059635a882b744a9e303c4f107a249fcc
George Koloidi <Battleaxe@bk.ru>
2023-10-10 19:24:49 +0200

    init: start project

---
+++ src/utils/recordAudio.ts
@@ -0,0 +1,49 @@
+import {nanoid} from 'nanoid';
+
+type AudioResult = {
+	audioFile: File;
+	audioUrl: string;
+	play: () => void;
+	audioName: string;
+};
+
+export default function recordAudio() {
+	return new Promise((resolve) => {
+		navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
+			const mediaRecorder = new MediaRecorder(stream);
+			const audioChunks: Blob[] = [];
+
+			mediaRecorder.addEventListener('dataavailable', (event) => {
+				audioChunks.push(event.data);
+			});
+
+			function start() {
+				mediaRecorder.start();
+			}
+
+			function stop(): Promise<AudioResult> {
+				return new Promise((resolve) => {
+					mediaRecorder.addEventListener('stop', () => {
+						const audioName = nanoid();
+						const audioFile = new File(audioChunks, audioName, {
+							type: 'audio/mpeg',
+						});
+						const audioUrl = URL.createObjectURL(audioFile);
+						const audio = new Audio(audioUrl);
+						function play() {
+							audio.play();
+						}
+						resolve({audioFile, audioUrl, play, audioName});
+					});
+
+					mediaRecorder.stop();
+					mediaRecorder.stream.getTracks().forEach((track) => {
+						track.stop();
+					});
+				});
+			}
+
+			resolve({start, stop});
+		});
+	});
+}
